acd7ea01ed5295440b5ea665b052f1f348aa5513
Jan Randa <randajan@email.cz>
2023-01-07 19:30:05 +0100

    bf: templates

--- src/templates.js
+++ src/templates.js
@@ -8,7 +8,7 @@ export default _=>({
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Supersimple start for developing full stack app with node.js" />
+<meta name="description" content="{{description}}" />
 <title>{{name}} v{{version}} by {{author}}</title>
 </head>
 
